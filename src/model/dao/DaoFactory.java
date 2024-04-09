c9dadacea3f7c81f1f5e8d584a8d8cf4427f5c35
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-08 21:40:35 -0300

    SellerDaoJBDC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,9 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJBDC;
+
+public class DaoFactory {
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJBDC();
+    }
+}
