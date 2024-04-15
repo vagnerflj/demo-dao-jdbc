a6df046229af014fe8ab59b22e28871256484348
Vagner ferreira <euvagnerferreira@gmail.com>
2024-04-15 10:04:11 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJBDC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDaoJBDC();
+        return new SellerDaoJBDC(DB.getConnection());
     }
 }
